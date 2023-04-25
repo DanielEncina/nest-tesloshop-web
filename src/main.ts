1bab50a1aed72b0a131bffbb93e6e1d28f7fab34
Daniel Encina <dencinaa@ripley.com>
2023-04-24 22:11:56 -0400

    end section 15

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import './style.css'
+import { connectToServer } from './socket-client.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h2>WebSocket - Client</h2>
+
+    <input id="jwt-token" placeholder="Json WebToken" />
+    <button id="btn-connect">Conect</button>
+
+    <br />
+    <span id="server-status"></span>
+
+    <ul id="clients-ul"></ul>
+
+    <form id="message-form">
+      <input placeholder="message" id="message-input" />
+    </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
+  </div>
+`
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+
+btnConnect.addEventListener('click', () => {
+  if (jwtToken.value.trim().length <= 0) return alert('JWT Token is required');
+
+  connectToServer(jwtToken.value.trim());
+});
